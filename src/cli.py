3f160a7fe5688ca18c544a37b3c43814e0b68a42
pwissenberg <paul.wissenberg@googlemail.com>
2022-11-24 21:21:25 +0100

    Cli + Cli testing can begin

--- src/cli.py
+++ src/cli.py
@@ -1,17 +1,28 @@
 import typer
-
+from .parser import Parser
+from .client import Client
 app = typer.Typer()
+download_app = typer.Typer()
+client = Client()
+parser = Parser()
 
 @app.command()
-def download():
-    #Create client
+def download(input: str, output_folder: str = './'):
+    '''Downloads every dataset from the input file
+
+    :param str input: Path to the file. Containing all f the links.
+    :param str output: Optional path to a folder to store all of the datasets.
+    '''
+    client.download_datasets_from_file(input, output_folder)
+
+@app.command()
+def concat():
     pass
 
-
-
 @app.command()
-def lol():
-    print('sqws')
+def complete_data_pip():
+    pass
+
 
 if __name__ == "__main__":
     app()
